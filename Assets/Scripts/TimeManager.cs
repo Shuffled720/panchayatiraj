93ebc30c9298b71b3a8ca70ca4686d95660b145e
Shuffled720 <shuffled720@gmail.com>
2023-12-19 16:37:42 +0530

    revert one

---
+++ Assets/Scripts/TimeManager.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TimeManager : MonoBehaviour
+{
+    // public int initialPopulation = 9910;
+
+    // public int yearElapsed = 0;
+
+    // public int currentPopulation = 9910;
+    // public int deathRate = 9.7/1000;
+    // public int birthRate = 16.97/1000;
+    // public LogicScript yearElapsed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void updatePopulation(){
+        
+    }
+}
